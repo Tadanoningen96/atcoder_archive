1bf36a91fb86738c9e473be9e902e40ee5816d98
Tadanoningen96 <fujikawa.taro@outlook.jp>
2021-11-28 02:12:47 +0900

    abc227,abc228 & abc229 is added

---
+++ atcoder/abc227/b/main.cpp
@@ -0,0 +1,118 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int (i)=0;(i) < (n);(i)++)
+#define rep2(i,m,n) for(int (i)=(m);(i) < (n);(i)++)
+
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+bool chmax(T &a,const T &b) {if(a < b) {a = b;return true;}return false;}
+template<typename T>
+bool chmin(T &a,const T &b) {if(a > b) {a = b;return true;}return false;}
+
+//Graph
+vector<int> G[100005];
+
+struct node{
+	int to;
+	ll cost;
+};
+vector<node> GC[100005];
+
+//Mathematics(gcd,lcm,power,factorization,etc..)
+template<typename T>
+T gcd(T a,T b){
+	if(b == 0) return a;
+	else return gcd(b,a%b);
+}
+
+template<typename T>
+T lcm(T a,T b){
+	T res = a;
+	res /= gcd(a,b);res *= b;
+	return res;
+}
+
+template<typename T>
+vector<T> divisor(T n){
+	vector<T> res;
+
+	for(int i=1;i*i <= n;i++){
+		if(n % i == 0){
+			res.push_back(i);
+			if(i * i != n) res.push_back(n/i);
+		}
+	}
+	return res;
+}
+
+template<typename T>
+map<T,T> factorization(T n){
+	map<T,T> res;
+	for(int i=0;i*i <= n;i++){
+		 while(n % i == 0){
+			res[i] += 1;
+			n /= i;
+		}
+	}
+	if(n != 1) res[n] += 1;
+	return res;
+}
+
+ll pow(ll x,ll n){
+	ll res = 1;
+	while(n > 0){
+		if(n & 1) res = res * x;
+		x = x*x;
+		n >>= 1;
+	}
+	return res;
+}
+
+ll mod_pow(ll x,ll n,ll mod){
+	ll res = 1;
+	while(n > 0){
+		if(n & 1) res = res * x % mod;
+		x = x*x % mod;
+		n >>= 1;
+	}
+	return res;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	vector<int> s(n);
+
+	for(int i=0;i < n;i++) cin >> s[i];
+
+	vector<int> m;
+
+	for(int a=1;a <= 1000;a++){
+		for(int b=a;b <= 1000;b++){
+			int S = 4*a*b + 3*a + 3*b;
+			if(S <= 1000) m.push_back(S);
+		}
+	}
+
+	sort(m.begin(),m.end());
+	m.erase(unique(m.begin(),m.end()),m.end());
+
+	int ans = 0;
+
+	for(int i=0;i < (int)s.size();i++){
+		bool flag = true;
+		for(int j=0;j < (int)m.size();j++){
+			if(s[i] == m[j]) {
+				flag = false;
+				break;
+			}
+		}
+		if(flag) ans++;
+	}
+		
+	cout << ans << endl;
+	return 0;
+}
---
+++ atcoder/abc229/b/main.cpp
@@ -0,0 +1,218 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int (i)=0;(i) < (n);(i)++)
+#define rep2(i,m,n) for(int (i)=(m);(i) < (n);(i)++)
+
+
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+template<typename T>
+bool chmax(T &a,const T &b) {if(a < b) {a = b;return true;}return false;}
+template<typename T>
+bool chmin(T &a,const T &b) {if(a > b) {a = b;return true;}return false;}
+
+//Graph
+vector<int> G[100005];
+
+struct node{
+	int to;
+	ll cost;
+};
+vector<node> GC[100005];
+
+//Mathematics(gcd,lcm,power,factorization,etc..)
+
+namespace math{
+	template<typename T>
+	T gcd(T a,T b){
+		if(b == 0) return a;
+		else return gcd(b,a%b);
+	}
+	template<typename T>
+	T lcm(T a,T b){
+		T res = a;
+		res /= gcd(a,b);res *= b;
+		return res;
+	}
+	template<typename T>
+	vector<T> divisor(T n){
+		vector<T> res;
+
+		for(int i=1;i*i <= n;i++){
+			if(n % i == 0){
+				res.push_back(i);
+				if(i * i != n) res.push_back(n/i);
+			}
+		}
+		return res;
+	}
+	template<typename T>
+	map<T,T> factorization(T n){
+		map<T,T> res;
+		for(int i=0;i*i <= n;i++){
+			 while(n % i == 0){
+				res[i] += 1;
+				n /= i;
+			}
+		}
+		if(n != 1) res[n] += 1;
+		return res;
+	}
+	ll pow(ll x,ll n){
+		ll res = 1;
+		while(n > 0){
+			if(n & 1) res = res * x;
+			x = x*x;
+			n >>= 1;
+		}
+		return res;
+	}
+	ll mod_pow(ll x,ll n,ll mod){
+		ll res = 1;
+		while(n > 0){
+			if(n & 1) res = res * x % mod;
+			x = x*x % mod;
+			n >>= 1;
+		}
+		return res;
+	}
+}
+//data structure
+namespace data{
+	class unionBySize{
+		private:
+		vector<int> par;
+		public:
+		unionBySize(int n) : par(n,-1){}
+
+		int find(int x){
+			if(par[x] < 0) return x;
+			else
+				return par[x] = find(par[x]);
+		}
+
+		bool unite(int x,int y){
+			x = find(x);y = find(y);
+			if(x == y) return false;
+			else{
+				if(par[x] < par[y]){
+					auto swap = [&x,&y](){
+						int tmp = x;
+						x = y;
+						y = tmp;
+					};
+					swap();
+				}
+				par[y] += par[x];
+				par[x] = y;
+				return true;
+			}
+		}
+
+		bool same(int x,int y){
+			return find(x) == find(y);
+		}
+
+		int size(int x){
+			return -par[find(x)];
+		}
+	};
+
+	class unionByRank{
+		private:
+		vector<int> par,rank;
+		public:
+		unionByRank(int n) : rank(n,0) {
+			for(int i=0;i < n;i++){
+				par[i] = i;
+			}
+		}
+
+		int find(int x){
+			if(par[x] == x) return x;
+			else return par[x] = find(par[x]);
+		}
+
+		bool unite(int x,int y){
+			x = find(x);
+			y = find(y);
+
+			if(x == y) return false;
+
+			if(rank[x] < rank[y]){
+				par[x] = y;
+			}else{
+				par[y] = x;
+				if(rank[x] == rank[y]) rank[x]++;
+			}
+
+			return true;
+		}
+
+		bool same(int x,int y) {
+			return find(x) == find(y);
+		}
+	};
+	
+	template<typename T>
+	class RMQ{
+		private:
+		int n;
+		const T INF = numeric_limits<T>::max();
+		vector<T> dat;
+		public:
+		RMQ(int n_): n(),dat(4*n_,INF) {
+			int x = 1;
+			while(n_ > x){
+				x *= 2;
+			}
+			n = x;
+		}
+
+		void update(int i,T x){
+			i += n-1;
+
+			while(i > 0){
+				i = (i-1)/2;
+				dat[i] = min(dat[2*i+1],dat[2*i+2]);
+			}
+		}
+
+		T query(int a,int b){return query_sub(a,b,0,0,n);};
+		T query_sub(int a,int b,int k,int l,int r){
+			if(r <= a || b >= l) return INF;
+			else if(a <= l && r <= b) return dat[k];
+			else{
+				T vl = query_sub(a,b,2*k+1,l,(l+r)/2);
+				T vr = query_sub(a,b,2*k+2,(l+r)/2 + 1,r);
+				return min(vl,vr);
+				}
+		}
+	};
+
+}
+
+int main(){
+	ll a,b;
+	vector<ll> da(19),db(19);
+	cin >> a >> b;
+	
+	for(int i=0;i < 19;i++){
+		da[i] = a % 10;
+		db[i] = b % 10;
+		a /= 10;
+		b /= 10;
+	}
+
+	reverse(da.begin(),da.end());
+	reverse(db.begin(),db.end());
+	
+	bool ok = true;
+	for(int i=0;i < 19;i++){
+		if(da[i] + db[i] >= 10) ok = false;
+	}
+
+	cout << (ok ? "Easy" : "Hard") << endl;
+	return 0;
+}
